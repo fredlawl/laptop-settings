b56d1ed6bf2bf32dbb28624e081ae33caa73183c
Frederick Lawler <fred@fredlawl.com>
2024-12-21 18:52:31 -0600

    fix previous dir lookup

--- dotfiles-i3-alacritty/.local/bin/term.sh
+++ dotfiles-i3-alacritty/.local/bin/term.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env -S bash
-exec alacritty --working-directory $(cat /tmp/run/user/`id -u`/wd)
-
+lookup_dir=/tmp/run/user/$(id -u)/wd
+dir=$(cat $lookup_dir)
+default="/home/$(id -un)"
+working_dir=${dir:-"$default"}
+exec alacritty --working-directory "$working_dir"
